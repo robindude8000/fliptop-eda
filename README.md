c1d6cffa473344c7eb510f673dc49394177ead49
robindude8000 <robindude8000>
2025-07-01 22:37:03 +0800

    done get metadata script

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# fliptop-eda
+# FlipTop Battle League EDA 🎤📊
+
+This is an exploratory data analysis (EDA) project focused on the **FlipTop Battle League**, the Philippines' premier rap battle platform. The goal is to collect data from their official YouTube channel and generate insights and statsheets around:
+
+- Emcees
+- Battles
+- Number of views
+- Upload dates
+- Other possible metrics (e.g., like counts, battle duration, comment count)
+
+---
+
+## 🔍 Objectives
+
+- Scrape battle data from FlipTop’s YouTube channel
+- Analyze views, upload frequency, and top-performing battles
+- Profile emcees based on participation and popularity (views)
+- Visualize insights using charts and tables
+- Create a reusable dataset for further NLP or ML experiments (will need further study for this one)
+
+---
+
+## 🛠 Tools & Technologies
+
+- **Python**
+- **Pandas**
+- **YouTube API**
+- **Matplotlib / Seaborn / Plotly**
+- **Jupyter Notebooks**
+- **Git & GitHub**
+
+---
+
+## 📁 Project Structure
+
+```bash
+fliptop-eda/
+│
+├── dataset/                
+├───── fliptop_videos.json  # Raw data in json          
+├───── fliptop_videos.csv   # Raw data in csv
+├── fliptop_eda.ipynb       # Jupyter notebooks for Cleaning, EDA, Graphs, charts, and visualizations 
+├── get_videos.py           # Python scripts for scraping      
+└── README.md               # Project overview
---
+++ get_videos.py
@@ -0,0 +1,94 @@
+from googleapiclient.discovery import build
+from tqdm import tqdm
+import json
+import pandas as pd
+import os
+
+# --- Config ---
+# replace this later with config/env handling
+API_KEY = "AIzaSyBL4g6pvIeqMCGKecIfY-W7Aq5eTLQCEuo"
+# fliptop channel id. This is generated with the help of a third party tool
+CHANNEL_ID = "UCBdHwFIE4AJWSa3Wxdu7bAQ"
+
+
+# --- Prepare output folder ---
+os.makedirs("dataset", exist_ok=True)
+
+
+# --- Initialize YouTube API client ---
+# build() is used to construct the Youtube API service object
+youtube = build("youtube", "v3", developerKey=API_KEY)
+
+
+# --- Get uploads playlist id ---
+channel_res = youtube.channels().list(
+    part="contentDetails",  # to extract just the playlist info
+    id=CHANNEL_ID
+).execute()
+
+uploads_playlist_id = channel_res["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+# print(f"Uploads Playlist ID: {uploads_playlist_id}")
+
+
+# --- Collect video IDs from playlist ---
+video_ids = []
+next_page = None # used to navigate pages
+
+while True:
+    pl_response = youtube.playlistItems().list(
+        part="snippet",
+        playlistId=uploads_playlist_id,
+        maxResults=50,
+        pageToken=next_page
+    ).execute()
+
+    for item in pl_response["items"]:
+        video_ids.append(item["snippet"]["resourceId"]["videoId"])
+
+    next_page = pl_response.get("nextPageToken")
+    if not next_page:
+        break
+    
+# --- Fetch metadata
+videos = []
+
+for i in range(0, len(video_ids), 50):
+    batch_ids = video_ids[i:i+50]
+
+    vid_response = youtube.videos().list(
+        part="snippet,statistics",
+        id=",".join(batch_ids)
+    ).execute()
+
+    for item in vid_response["items"]:
+        stats = item["statistics"]
+        snippet = item["snippet"]
+
+        videos.append({
+            "video_id": item["id"],
+            "title": snippet["title"],
+            "description": snippet["description"],
+            "publishedAt": snippet["publishedAt"],
+            "view_count": stats["viewCount"],
+            "like_count": stats["likeCount"],
+            "comment_count": stats["commentCount"]
+        })
+
+'''
+# Preview first 5 videos
+for vid in videos[:3]:
+    print(vid)
+'''
+
+# --- Save to a JSON file ---
+with open("dataset/fliptop_videos.json", "w", encoding="utf-8") as f:
+    json.dump(videos, f, ensure_ascii=False, indent=4)
+
+# --- Save to csv file also ---
+df = pd.DataFrame(videos)
+df.to_csv("dataset/fliptop_videos.csv", index=False, encoding="utf-8")
+
+
+print(f"✅ Done! Saved {len(videos)} videos to:")
+print(f"Saved {len(videos)} videos to 'dataset/fliptop_videos.json'")
+print(f"Saved {len(df)} videos to 'dataset/fliptop_videos.csv'")
